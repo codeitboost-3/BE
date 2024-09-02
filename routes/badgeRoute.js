4d50b88e4e98179aaf4d9ea2c3fc038398faa3bd
JungHyunHann <jade20031229@gmail.com>
2024-09-02 22:25:54 +0900

    오류 6차 수정

--- routes/badgeRoute.js
+++ routes/badgeRoute.js
@@ -3,7 +3,7 @@ import { getGroupBadges } from '../controllers/groupController.js';
 
 const router = express.Router();
 
-// 그룹 배지 조회
+// 그룹 배지 조회--
 router.get('/:groupId', getGroupBadges);
 
 export default router;
